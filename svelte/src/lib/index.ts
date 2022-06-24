f41dc0dcd360fb460675f6257c35aca5012733b1
mateuszpiorowski <mateuszpiorowski@gmail.com>
2022-06-24 18:48:30 +0200

    fix

--- svelte/src/lib/index.ts
+++ svelte/src/lib/index.ts
@@ -1,14 +1,14 @@
-export { default as Button } from './@components/button.svelte';
-export { default as Checkbox } from './@components/checkbox.svelte';
-export { default as CheckboxGroup } from './@components/checkboxGroup.svelte';
-export { default as Drawer } from './@components/drawer/drawer.svelte';
-export { default as Dropdown } from './@components/dropdown.svelte';
-export { default as Input } from './@components/input.svelte';
-export * from './@components/pagination/pagination.helper';
-export { default as Pagination } from './@components/pagination/pagination.svelte';
-export { default as Select } from './@components/select.svelte';
-export { default as SimpleInput } from './@components/simpleInput.svelte';
-export { default as Textarea } from './@components/textarea.svelte';
-export * from './@components/toast/toast.helper';
-export { default as Toast } from './@components/toast/toast.svelte';
+export { default as Button } from './button.svelte';
+export { default as Checkbox } from './checkbox.svelte';
+export { default as CheckboxGroup } from './checkboxGroup.svelte';
+export { default as Drawer } from './drawer/drawer.svelte';
+export { default as Dropdown } from './dropdown.svelte';
+export { default as Input } from './input.svelte';
+export * from './pagination/pagination.helper';
+export { default as Pagination } from './pagination/pagination.svelte';
+export { default as Select } from './select.svelte';
+export { default as SimpleInput } from './simpleInput.svelte';
+export { default as Textarea } from './textarea.svelte';
+export * from './toast/toast.helper';
+export { default as Toast } from './toast/toast.svelte';
 
