7faf50a2380cb512ac41548b36189083d11dac0a
mateuszpiorowski <mateuszpiorowski@gmail.com>
2022-06-25 11:28:13 +0200

    missing-modal

--- svelte/src/lib/index.ts
+++ svelte/src/lib/index.ts
@@ -4,6 +4,7 @@ export { default as CheckboxGroup } from './checkboxGroup.svelte';
 export { default as Drawer } from './drawer/drawer.svelte';
 export { default as Dropdown } from './dropdown.svelte';
 export { default as Input } from './input.svelte';
+export { default as Moda } from './modal.svelte';
 export * from './pagination/pagination.helper';
 export { default as Pagination } from './pagination/pagination.svelte';
 export { default as Select } from './select.svelte';
