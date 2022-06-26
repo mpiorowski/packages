a9dffbc61ba3dbf926bc72f50d185e90bf447da8
mateuszpiorowski <mateuszpiorowski@gmail.com>
2022-06-26 11:23:21 +0200

    move-index

--- utils/lib/index.ts
+++
@@ -1,6 +0,0 @@
-import { assertIsDefined } from './assertIsDefined';
-import { checkEnv } from './checkEnv';
-import { clickOutside } from './clickOutside';
-import { searchArray } from './searchArray';
-
-export { assertIsDefined, checkEnv, searchArray, clickOutside };
