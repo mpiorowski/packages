2df4e6f59f87c46b94083bb211c305fa6054d314
mateuszpiorowski <mateuszpiorowski@gmail.com>
2022-06-26 11:18:05 +0200

    exports

--- utils/index.ts
+++
@@ -1,6 +0,0 @@
-import { assertIsDefined } from './src/assertIsDefined';
-import { checkEnv } from './src/checkEnv';
-import { clickOutside } from './src/clickOutside';
-import { searchArray } from './src/searchArray';
-
-export { assertIsDefined, checkEnv, searchArray, clickOutside };
---
+++ utils/lib/index.ts
@@ -0,0 +1,6 @@
+import { assertIsDefined } from './assertIsDefined';
+import { checkEnv } from './checkEnv';
+import { clickOutside } from './clickOutside';
+import { searchArray } from './searchArray';
+
+export { assertIsDefined, checkEnv, searchArray, clickOutside };
