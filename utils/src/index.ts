c30cf233490bda043733b390bc0b6ff602e3e358
mateuszpiorowski <mateuszpiorowski@gmail.com>
2022-06-28 22:42:18 +0200

    fix

--- utils/src/index.ts
+++ utils/src/index.ts
@@ -1,3 +1,6 @@
+// import pkg from '../package.json';
+// console.log(`running version ${pkg.version}`);
+
 export * from './lib/assertIsDefined';
 export * from './lib/checkEnv';
 export * from './lib/clickOutside';
