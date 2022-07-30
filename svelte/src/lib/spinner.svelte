ca1694627dfe7cce7424d3cc79ae64d41ae609fd
mateuszpiorowski <mateuszpiorowski@gmail.com>
2022-07-31 00:23:16 +0200

    fix-spinner

--- svelte/src/lib/spinner.svelte
+++ svelte/src/lib/spinner.svelte
@@ -5,23 +5,14 @@
 
 <div class:center>
   <svg
-    class="spinner"
     width={size}
     height={size}
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
   >
-    <circle
-      class="opacity-25"
-      cx="12"
-      cy="12"
-      r="10"
-      stroke="currentColor"
-      stroke-width="4"
-    />
+    <circle cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
     <path
-      class="opacity-75"
       fill="currentColor"
       d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
     />
@@ -29,9 +20,6 @@
 </div>
 
 <style>
-  .spinner {
-    @apply animate-spin text-white;
-  }
   .center {
     position: absolute;
     top: 0;
@@ -41,5 +29,14 @@
     display: flex;
     justify-content: center;
     align-items: center;
+  }
+  svg {
+    @apply animate-spin text-white;
+  }
+  circle {
+    @apply opacity-25;
+  }
+  path {
+    @apply opacity-75;
   }
 </style>
